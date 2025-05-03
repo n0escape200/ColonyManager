4d27b19e0fdc09b85d377013ff36b77968ced529
n0escape200 <n0_escape200@yahoo.com>
2025-05-03 17:48:32 +0300

    Added trees

---
+++ Assets/GameAssets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject npcPrefab; 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
